4d2995a789fe475abe2ac003506f6819fb94227f
muthral <152712762+muthral@users.noreply.github.com>
2026-04-09 08:34:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from game_tebak import angka, stoptebak, angkachaos, stopchaos, angkaduel, joind
 from game_spy import spy, join, startspy, vote, pemain, stopspy, skip
 from game_slot import slot, kekayaan
 from game_shop import shop, beli
+from admin import setsaldo, addsaldo, setscore, addscore
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -51,6 +52,12 @@ if __name__ == "__main__":
     app.add_handler(CommandHandler("pemain", pemain))
     app.add_handler(CommandHandler("stopspy", stopspy))
     app.add_handler(CommandHandler("skip", skip))
+
+    # Admin commands
+    app.add_handler(CommandHandler("setsaldo", setsaldo))
+    app.add_handler(CommandHandler("addsaldo", addsaldo))
+    app.add_handler(CommandHandler("setscore", setscore))
+    app.add_handler(CommandHandler("addscore", addscore))
 
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, track_member))
 
